712d7cd49db7e0a3f1349a2b2c08f7a671d51ccd
irynailliukhina <irynailliukhina@gmail.com>
2020-11-05 21:23:22 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//Active header after scroll
+
+window.addEventListener('scroll', function () {
+    yOffset = window.pageYOffset;
+    let header = document.querySelector('.header')
+    if (yOffset > 24) {
+        header.classList.add("header-scrolled")
+    } else {
+        header.classList.remove("header-scrolled")
+    }
+});
+
+// Burger menu
+// document.querySelectorAll(`.search-icon`).forEach((item) =>
+//     item.addEventListener('click', () => {
+
+//         if (item.classList.contains(`search-icon--active`)) {
+//             item.classList.remove(`search-icon--active`)
+//             item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#search"></use></svg>'
+//             document.getElementById("search").style.display = "none";
+//         } else {
+//             item.classList.toggle(`search-icon--active`)
+//             item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#cross"></use></svg>'
+//             document.getElementById("search").style.display = "block";
+//         }
+// }))
+
+
+//Accordion
+
+document.querySelectorAll(`.links__title`).forEach((item) =>
+    item.addEventListener(`click`, () => {
+        const parent = item.parentNode;
+
+        if (parent.classList.contains(`links__group--active`)) {
+            parent.classList.remove(`links__group--active`)
+        } else {
+            document.querySelectorAll(`links__group`).forEach((item) =>
+                item.classList.remove(`links__group--active`))
+            parent.classList.toggle(`links__group--active`)
+        }
+    })
+)
+
+//Search
+
+document.querySelectorAll(`.search-icon`).forEach((item) =>
+    item.addEventListener('click', () => {
+
+        if (item.classList.contains(`search-icon--active`)) {
+            item.classList.remove(`search-icon--active`)
+            item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#search"></use></svg>'
+            document.getElementById("search").style.display = "none";
+        } else {
+            item.classList.toggle(`search-icon--active`)
+            item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#cross"></use></svg>'
+            document.getElementById("search").style.display = "block";
+        }
+
+    }))

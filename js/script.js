f05bffa9bfd7568d2f2c5c15182ef4cdc25c8493
irynailliukhina <irynailliukhina@gmail.com>
2020-11-14 20:15:27 +0200

    js refactored and search blox fixed

--- js/script.js
+++ js/script.js
@@ -1,57 +1,62 @@
 //Active header after scroll
 
-window.addEventListener('scroll', function () {
-    yOffset = window.pageYOffset;
+window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset
     let header = document.querySelector('.header')
-    if (yOffset > 24) {
-        header.classList.add("header-scrolled")
-    } else {
-        header.classList.remove("header-scrolled")
-    }
+    yOffset > 24 ? header.classList.add("header-scrolled") : header.classList.remove("header-scrolled")
 });
 
 // Burger menu
-document.querySelectorAll(`.block-menu__toggler`).forEach((item) =>
-    item.addEventListener('click', () => {
-        document.getElementById("menu--mob").style.display = "block";
-    }
-    ))
-document.querySelectorAll(`.mob-cross`).forEach((item) =>
-    item.addEventListener('click', () => {
-        document.getElementById("menu--mob").style.display = "none";
-    }))
-
+const toggler = document.querySelector(`.block-menu__toggler`)
+toggler.addEventListener('click', () => {
+    auroraMethods.showMenu()
+})
+document.querySelector(`.mob-cross`).addEventListener('click', () => {
+    auroraMethods.hideMenu()
+})
 
 //Accordion
 
 document.querySelectorAll(`.links__title`).forEach((item) =>
     item.addEventListener(`click`, () => {
+        const activeGroup = `links__group--active`
         const parent = item.parentNode;
 
-        if (parent.classList.contains(`links__group--active`)) {
-            parent.classList.remove(`links__group--active`)
+        if (parent.classList.contains(activeGroup)) {
+            parent.classList.remove(activeGroup)
         } else {
             document.querySelectorAll(`.links__group`).forEach((child) =>
-                child.classList.remove(`links__group--active`))
-            parent.classList.add(`links__group--active`)
+                child.classList.remove(activeGroup))
+            parent.classList.add(activeGroup)
         }
     })
 )
 
-
 //Search
+const searchIcon = document.querySelector(`.search-icon`)
+const activeSearch = `search-icon--active`
 
-document.querySelectorAll(`.search-icon`).forEach((item) =>
-    item.addEventListener('click', () => {
 
-        if (item.classList.contains(`search-icon--active`)) {
-            item.classList.remove(`search-icon--active`)
-            item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#search"></use></svg>'
-            document.getElementById("search").style.display = "none";
-        } else {
-            item.classList.toggle(`search-icon--active`)
-            item.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#cross"></use></svg>'
-            document.getElementById("search").style.display = "block";
-        }
+searchIcon.addEventListener('click', () => {
+    if (searchIcon.classList.contains(activeSearch)) {
+        auroraMethods.hideSearch()
+    } else {
+        auroraMethods.showSearch()
+    }
+})
 
-    }))
+const auroraMethods = {
+    hideSearch: () => {
+        searchIcon.classList.remove(activeSearch);
+        searchIcon.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#search"></use></svg>'
+        document.getElementById("search").style.display = "none"
+    },
+    showSearch: () => {
+        searchIcon.classList.toggle(activeSearch)
+        searchIcon.innerHTML = '<svg class="search-icon__img"><use xlink:href="img/sprite.svg#cross"></use></svg>'
+        document.getElementById("search").style.display = "block"
+    },
+    showMenu: () => document.getElementById("menu--mob").style.display = "block",
+    hideMenu: () => document.getElementById("menu--mob").style.display = "none",
+
+}
